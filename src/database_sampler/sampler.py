06bea5b924cb55e74507f802770cd1036fdfc59a
Alex Carimán <alex@cariman.cl>
2025-02-23 22:05:40 -0300

    feat: adds filtered query based on datetime

--- src/database_sampler/sampler.py
+++ src/database_sampler/sampler.py
@@ -1,5 +1,5 @@
-
 from string import Template
+from datetime import datetime
 
 import polars as pl
 
@@ -7,11 +7,11 @@ from database_sampler.files import get_settings
 from database_sampler.conn import create_uri
 
 
-# TODO: enable filtering
-QUERY = Template("SELECT * FROM $entity_name")
+QUERY_ALL = Template("SELECT * FROM $name")
+QUERY_FILTERED = Template("SELECT * FROM $name WHERE $column BETWEEN '$start' AND '$end'")
 
 
-def run(settings: str = "etc/settings.yml"):
+def execute(settings: str = "etc/settings.yml", start: datetime = None, end: datetime = None):
     global_settings = get_settings(settings)
 
     for oper_name, oper_settings in global_settings["operations"].items():
@@ -19,17 +19,21 @@ def run(settings: str = "etc/settings.yml"):
         dst_uri = create_uri(global_settings["connections"][oper_settings["destination"]])
 
         for entity in oper_settings["entities"]:
-            if "schema" in entity:
-                full_name = f"{entity['schema']}.{entity['name']}"
+            entity["name"] = f"{entity['schema']}.{entity['name']}" if "schema" in entity else entity["name"]
+
+            if "column" in entity:
+                query = QUERY_FILTERED
+                
+                # TODO: enable different combinations
+                # TODO: enable epoch timestamps
+                entity["start"] = start 
+                entity["end"] = end
+
             else:
-                full_name = entity['name']
+                query = QUERY_ALL
 
             # pulls data
-            res = pl.read_database_uri(query=QUERY.substitute(entity_name=full_name), uri=src_uri, engine="adbc")
+            res = pl.read_database_uri(query=query.substitute(entity), uri=src_uri, engine="adbc")
 
             # push data
             res.write_database(table_name=entity['name'], connection=dst_uri, engine="adbc")
-
-
-if __name__ == "__main__":
-    run()
--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -1,6 +1,10 @@
+from datetime import datetime
 
-from database_sampler.sampler import run
+from database_sampler.sampler import execute
 
 
 def test_download(setup_database):
-    run()
+    start = datetime(2009, 1, 1)
+    end = datetime(2009, 1, 30)
+
+    execute(start=start, end=end)
